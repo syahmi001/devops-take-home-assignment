615885c1ae50a96fa4112063b71e03f3c7fb5a03
Muhammad Syahmi <50522796+syahmi001@users.noreply.github.com>
2022-05-14 14:43:17 +0800

    Adding terraform config files

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "ARM_CLIENT_ID" {
+  description = "Azure Kubernetes Service Cluster service principal"
+}
+
+variable "ARM_CLIENT_SECRET" {
+  description = "Azure Kubernetes Service Cluster password"
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+  description = "Azure Kubernetes Service Cluster subscriptionID"
+}
+
+variable "ARM_TENANT_ID" {
+  description = "Azure Kubernetes Service Cluster tenant ID"
+}
+

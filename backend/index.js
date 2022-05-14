4b374147b3ff4aceca657336f826dc7f2b46fe03
Muhammad Syahmi <50522796+syahmi001@users.noreply.github.com>
2022-05-14 22:41:55 +0800

    Configured the apps to work in kubernetes

--- backend/index.js
+++ backend/index.js
@@ -27,22 +27,22 @@ async function main() {
 
   app.use(cors());
 
-  app.get("/orders", async (req, res) => {
+  app.get("/api/orders", async (req, res) => {
     const orders = await orderCollection.find({}).toArray();
     res.send(orders);
   });
 
-  app.get("/counter", async (req, res) => {
+  app.get("/api/counter", async (req, res) => {
     const counter = await counterCollection.find({}).toArray();
     res.send(counter);
   });
 
-  app.delete("/orders/:id", async (req, res) => {
+  app.delete("/api/orders/:id", async (req, res) => {
     await orderCollection.deleteOne({ _id: Number(req.params.id) });
     res.send("ok");
   });
 
-  app.post("/orders", async (req, res) => {
+  app.post("/api/orders", async (req, res) => {
     const orderSeq = await counterCollection.findOne({ _id: "orderSeq" });
     await orderCollection.insertOne({
       _id: orderSeq.seq++,
